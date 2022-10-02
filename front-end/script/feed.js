6c05cf7dd7288ae767ec86f07e8a9e9d43d7ab75
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-02 12:57:16 +0300

    frontend: add feed popup js

--- front-end/script/feed.js
+++ front-end/script/feed.js
@@ -1,8 +1,17 @@
 const featherUserEl = document.querySelector(".feather-user");
 const editProfileEl = document.querySelector(".edit-profile");
 const backdropEl = document.querySelector(".backdrop");
+const saveEl = document.querySelector(".edit-profile-nav p");
 
 featherUserEl.addEventListener("click", () => {
   editProfileEl.classList.remove("none");
   backdropEl.classList.remove("none");
 });
+
+const closePopup = () => {
+  editProfileEl.classList.add("none");
+  backdropEl.classList.add("none");
+};
+
+saveEl.addEventListener("click", closePopup);
+backdropEl.addEventListener("click", closePopup);
