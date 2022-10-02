c63844fde00f691881a811de0fc703e401a84622
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-02 23:46:00 +0300

    frontend: add feed search

--- front-end/script/feed.js
+++ front-end/script/feed.js
@@ -3,9 +3,6 @@ const editProfileEl = document.querySelector(".edit-profile");
 const backdropEl = document.querySelector(".backdrop");
 const saveEl = document.querySelector(".edit-profile-nav p");
 const closeEl = document.querySelector(".edit-profile-nav svg");
-const messageiconEl = document.querySelector(".feather-message-circle");
-const rightSectionEl = document.querySelector(".right-section");
-const leftSectionEl = document.querySelector(".left-section");
 
 featherUserEl.addEventListener("click", () => {
   editProfileEl.classList.remove("none");
@@ -20,12 +17,3 @@ const closePopup = () => {
 saveEl.addEventListener("click", closePopup);
 backdropEl.addEventListener("click", closePopup);
 closeEl.addEventListener("click", closePopup);
-
-// messageiconEl.addEventListener("click", () => {
-//   if (window.innerWidth >= 800) {
-
-//   }
-//   leftSectionEl.classList.toggle("none");
-//   rightSectionEl.classList.toggle("block");
-//   console.log(window.innerWidth);
-// });
