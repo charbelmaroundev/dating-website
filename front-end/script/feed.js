0ea9bfd0447d0fc088f07e3b685ff59208d07c4e
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-02 12:52:11 +0300

    frontend: add feed popup save button

--- front-end/script/feed.js
+++ front-end/script/feed.js
@@ -1,3 +1,8 @@
 const featherUserEl = document.querySelector(".feather-user");
+const editProfileEl = document.querySelector(".edit-profile");
+const backdropEl = document.querySelector(".backdrop");
 
-featherUserEl.addEventListener("click", () => {});
+featherUserEl.addEventListener("click", () => {
+  editProfileEl.classList.remove("none");
+  backdropEl.classList.remove("none");
+});
