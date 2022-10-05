0d4421358ca380328697b53ac98f39e93443afc8
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-10-05 17:01:46 +0300

    frontend: add signup button eventlistener conditions

--- front-end/script/signup.js
+++ front-end/script/signup.js
@@ -6,7 +6,21 @@ const interstedGenderEl = document.getElementById("intersted-gender");
 const locationEl = document.getElementById("location");
 const passwordEl = document.getElementById("password");
 const submitEl = document.getElementById("submit");
+const responseEl = document.querySelector(".response");
 
 submitEl.addEventListener("click", (e) => {
   e.preventDefault();
+  if (
+    !firstNameEl.value ||
+    !lastNameEl.value ||
+    !usernameEl.value ||
+    !genderEl.value ||
+    !interstedGenderEl.value ||
+    !locationEl.value ||
+    !passwordEl.value
+  ) {
+    console.log("haha");
+    responseEl.style.color = "#FFB4B4";
+    responseEl.style.opacity = 1;
+  }
 });
